b998385c1c7f01b2e1a01fc2a874c33c400904e8
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-01-29 19:41:18 +0300

    Conmplete push from local

---
+++ client/src/components/common-form/index.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import { Button } from "../ui/button";
+import FormControls from "./form-controls";
+
+function CommonForm({
+  // eslint-disable-next-line react/prop-types
+  handleSubmit,
+  buttonText,
+  formControls = [],
+  formData,
+  setFormData,
+  isButtonDisabled = false,
+}) {
+  return (
+    <form onSubmit={handleSubmit}>
+      {/* render form controls here */}
+      <FormControls
+        formControls={formControls}
+        formData={formData}
+        setFormData={setFormData}
+      />
+      <Button disabled={isButtonDisabled} type="submit" className="mt-5 w-full">
+        {buttonText || "Submit"}
+      </Button>
+    </form>
+  );
+}
+
+export default CommonForm;
